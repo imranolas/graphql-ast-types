230bb52bec91925aeb120f5e98f88455ef10ffc7
imranolas <imran.sulemanji@gmail.com>
2017-10-11 12:06:24 +0100

    Refactor type generation

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// import toFastProperties from 'to-fast-properties';
-import loClone from 'lodash/clone';
-import uniq from 'lodash/uniq';
+// @flow
 
-const t = exports;
+import loClone from 'lodash/clone';
+import './definitions/graphql';
+
+import { ALIAS_KEYS, NODE_FIELDS, BUILDER_KEYS } from './definitions';
+
+const t = exports; // Maps all exports to t
 
 /**
  * Registers `is[Type]` and `assert[Type]` generated functions for a given `type`.
@@ -12,12 +15,12 @@ const t = exports;
 function registerType(type: string) {
   let is = t[`is${type}`];
   if (!is) {
-    is = t[`is${type}`] = function(node, opts) {
+    is = t[`is${type}`] = function is(node, opts) {
       return t.is(type, node, opts);
     };
   }
 
-  t[`assert${type}`] = function(node, opts) {
+  t[`assert${type}`] = function assert(node, opts) {
     opts = opts || {};
     if (!is(node, opts)) {
       throw new Error(`Expected type ${JSON.stringify(type)} with option ${JSON.stringify(opts)}`);
@@ -25,39 +28,6 @@ function registerType(type: string) {
   };
 }
 
-//
-
-// export {
-//   STATEMENT_OR_BLOCK_KEYS,
-//   FLATTENABLE_KEYS,
-//   FOR_INIT_KEYS,
-//   COMMENT_KEYS,
-//   LOGICAL_OPERATORS,
-//   UPDATE_OPERATORS,
-//   BOOLEAN_NUMBER_BINARY_OPERATORS,
-//   EQUALITY_BINARY_OPERATORS,
-//   COMPARISON_BINARY_OPERATORS,
-//   BOOLEAN_BINARY_OPERATORS,
-//   NUMBER_BINARY_OPERATORS,
-//   BINARY_OPERATORS,
-//   BOOLEAN_UNARY_OPERATORS,
-//   NUMBER_UNARY_OPERATORS,
-//   STRING_UNARY_OPERATORS,
-//   UNARY_OPERATORS,
-//   INHERIT_KEYS,
-//   BLOCK_SCOPED_SYMBOL,
-//   NOT_LOCAL_BINDING
-// } from './constants';
-
-import './definitions/graphql';
-
-import {
-  // VISITOR_KEYS,
-  ALIAS_KEYS,
-  NODE_FIELDS,
-  BUILDER_KEYS
-  // DEPRECATED_KEYS
-} from './definitions';
 export { ALIAS_KEYS, NODE_FIELDS, BUILDER_KEYS };
 
 /**
@@ -67,14 +37,6 @@ export { ALIAS_KEYS, NODE_FIELDS, BUILDER_KEYS };
 for (const type in t.VISITOR_KEYS) {
   registerType(type);
 }
-
-// Compat helpers so code for Babel 6.x is more likely to work on Babel 7.x.
-// export function isRestProperty(...args) {
-//   return t.isRestElement(...args);
-// }
-// export function isSpreadProperty(...args) {
-//   return t.isSpreadElement(...args);
-// }
 
 /**
  * Flip `ALIAS_KEYS` for faster access in the reverse direction.
@@ -155,39 +117,32 @@ export function isType(nodeType: string, targetType: string): boolean {
 }
 
 /**
- * Description
+ * For each call of #defineType, the following expression evalutates and generates
+ * a builder function that validates incoming arguments and returns a valid AST node.
  */
 
 Object.keys(t.BUILDER_KEYS).forEach(type => {
   const keys = t.BUILDER_KEYS[type];
 
-  function builder() {
-    if (arguments.length > keys.length) {
+  function builder(...args) {
+    if (args.length > keys.length) {
       throw new Error(
-        `t.${type}: Too many arguments passed. Received ${arguments.length} but can receive ` +
+        `t.${type}: Too many arguments passed. Received ${args.length} but can receive ` +
           `no more than ${keys.length}`
       );
     }
 
-    const node = {};
-    node.type = type;
+    const node = keys.reduce(
+      (node, key, i) => {
+        let arg = args[i] || t.NODE_FIELDS[type][key].default;
+        return Object.assign({ [key]: arg }, node);
+      },
+      { kind: type }
+    );
 
-    let i = 0;
-
-    for (const key of keys) {
-      const field = t.NODE_FIELDS[type][key];
-
-      let arg = arguments[i++];
-      if (arg === undefined) {
-        arg = loClone(field.default);
-      }
-
-      node[key] = arg;
+    for (const key in node) {
+      validate(node, key, node[key]);
     }
-
-    // for (const key in node) {
-    //   validate(node, key, node[key]);
-    // }
 
     return node;
   }
@@ -197,450 +152,42 @@ Object.keys(t.BUILDER_KEYS).forEach(type => {
 });
 
 /**
- * Description
+ * Executes the field validators for a given node
  */
 
-// for (const type in t.DEPRECATED_KEYS) {
-//   const newType = t.DEPRECATED_KEYS[type];
-
-//   function proxy(fn) {
-//     return function () {
-//       console.trace(`The node type ${type} has been renamed to ${newType}`);
-//       return fn.apply(this, arguments);
-//     };
-//   }
-
-//   t[type] = t[type[0].toLowerCase() + type.slice(1)] = proxy(t[newType]);
-//   t[`is${type}`] = proxy(t[`is${newType}`]);
-//   t[`assert${type}`] = proxy(t[`assert${newType}`]);
-// }
-
-// /**
-//  * Description
-//  */
-
-// export function validate(node?: Object, key: string, val: any) {
-//   if (!node) {
-//     return;
-//   }
-
-//   const fields = t.NODE_FIELDS[node.type];
-//   if (!fields) {
-//     return;
-//   }
-
-//   const field = fields[key];
-//   if (!field || !field.validate) {
-//     return;
-//   }
-//   if (field.optional && val == null) {
-//     return;
-//   }
-
-//   field.validate(node, key, val);
-// }
-
-// /**
-//  * Test if an object is shallowly equal.
-//  */
-
-// export function shallowEqual(actual: Object, expected: Object): boolean {
-//   const keys = Object.keys(expected);
-
-//   for (const key of keys) {
-//     if (actual[key] !== expected[key]) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// }
-
-// /**
-//  * Append a node to a member expression.
-//  */
-
-// export function appendToMemberExpression(
-//   member: Object,
-//   append: Object,
-//   computed?: boolean
-// ): Object {
-//   member.object = t.memberExpression(member.object, member.property, member.computed);
-//   member.property = append;
-//   member.computed = !!computed;
-//   return member;
-// }
-
-// /**
-//  * Prepend a node to a member expression.
-//  */
-
-// export function prependToMemberExpression(member: Object, prepend: Object): Object {
-//   member.object = t.memberExpression(prepend, member.object);
-//   return member;
-// }
-
-// /**
-//  * Ensure the `key` (defaults to "body") of a `node` is a block.
-//  * Casting it to a block if it is not.
-//  */
-
-// export function ensureBlock(node: Object, key: string = 'body'): Object {
-//   return node[key] = t.toBlock(node[key], node);
-// }
-
-// /**
-//  * Create a shallow clone of a `node` excluding `_private` properties.
-//  */
-
-// export function clone(node: Object): Object {
-//   if (!node) {
-//     return node;
-//   }
-//   const newNode = {};
-//   for (const key in node) {
-//     if (key[0] === '_') {
-//       continue;
-//     }
-//     newNode[key] = node[key];
-//   }
-//   return newNode;
-// }
-
-// /**
-//  * Create a shallow clone of a `node` excluding `_private` and location properties.
-//  */
-
-// export function cloneWithoutLoc(node: Object): Object {
-//   const newNode = clone(node);
-//   newNode.loc = null;
-//   return newNode;
-// }
-
-// /**
-//  * Create a deep clone of a `node` and all of it's child nodes
-//  * exluding `_private` properties.
-//  */
-
-// export function cloneDeep(node: Object): Object {
-//   if (!node) {
-//     return node;
-//   }
-//   const newNode = {};
-
-//   for (const key in node) {
-//     if (key[0] === '_') {
-//       continue;
-//     }
-
-//     let val = node[key];
-
-//     if (val) {
-//       if (val.type) {
-//         val = t.cloneDeep(val);
-//       } else if (Array.isArray(val)) {
-//         val = val.map(t.cloneDeep);
-//       }
-//     }
-
-//     newNode[key] = val;
-//   }
-
-//   return newNode;
-// }
-
-// /**
-//  * Determines whether or not the input node `member` matches the
-//  * input `match`.
-//  *
-//  * For example, given the match `React.createClass` it would match the
-//  * parsed nodes of `React.createClass` and `React["createClass"]`.
-//  */
-
-// export function matchesPattern(
-//   member: Object,
-//   match: string | Array<string>,
-//   allowPartial?: boolean
-// ): boolean {
-//   // not a member expression
-//   if (!t.isMemberExpression(member)) {
-//     return false;
-//   }
-
-//   const parts = Array.isArray(match) ? match : match.split('.');
-//   const nodes = [];
-
-//   let node;
-//   for (node = member; t.isMemberExpression(node); node = node.object) {
-//     nodes.push(node.property);
-//   }
-//   nodes.push(node);
-
-//   if (nodes.length < parts.length) {
-//     return false;
-//   }
-//   if (!allowPartial && nodes.length > parts.length) {
-//     return false;
-//   }
-
-//   for (let i = 0, j = nodes.length - 1; i < parts.length; i++, j--) {
-//     const node = nodes[j];
-//     let value;
-//     if (t.isIdentifier(node)) {
-//       value = node.name;
-//     } else if (t.isStringLiteral(node)) {
-//       value = node.value;
-//     } else {
-//       return false;
-//     }
-
-//     if (parts[i] !== value) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// }
-
-// /**
-//  * Build a function that when called will return whether or not the
-//  * input `node` `MemberExpression` matches the input `match`.
-//  *
-//  * For example, given the match `React.createClass` it would match the
-//  * parsed nodes of `React.createClass` and `React["createClass"]`.
-//  */
-
-// export function buildMatchMemberExpression(
-//   match: string,
-//   allowPartial?: boolean
-// ): Object => boolean {
-//   const parts = match.split('.');
-//   return function (member) {
-//     return matchesPattern(member, parts, allowPartial);
-//   };
-// }
-
-// /**
-//  * Add comment of certain type to a node.
-//  */
-
-// export function addComment(node: Object, type: string, content: string, line?: boolean): Object {
-//   addComments(node, type, [
-//     {
-//       type: line ? 'CommentLine' : 'CommentBlock',
-//       value: content
-//     }
-//   ]);
-// }
-
-// /**
-//  * Add comments of certain type to a node.
-//  */
-
-// export function addComments(node: Object, type: string, comments: Array): Object {
-//   if (!comments || !node) {
-//     return;
-//   }
-
-//   const key = `${type}Comments`;
-
-//   if (node[key]) {
-//     if (type === 'leading') {
-//       node[key] = comments.concat(node[key]);
-//     } else {
-//       node[key] = node[key].concat(comments);
-//     }
-//   } else {
-//     node[key] = comments;
-//   }
-//   return node;
-// }
-
-// /**
-//  * Remove comment properties from a node.
-//  */
-
-// export function removeComments(node: Object): Object {
-//   for (const key of t.COMMENT_KEYS) {
-//     node[key] = null;
-//   }
-//   return node;
-// }
-
-// /**
-//  * Inherit all unique comments from `parent` node to `child` node.
-//  */
-
-// export function inheritsComments(child: Object, parent: Object): Object {
-//   inheritTrailingComments(child, parent);
-//   inheritLeadingComments(child, parent);
-//   inheritInnerComments(child, parent);
-//   return child;
-// }
-
-// export function inheritTrailingComments(child: Object, parent: Object) {
-//   _inheritComments('trailingComments', child, parent);
-// }
-
-// export function inheritLeadingComments(child: Object, parent: Object) {
-//   _inheritComments('leadingComments', child, parent);
-// }
-
-// export function inheritInnerComments(child: Object, parent: Object) {
-//   _inheritComments('innerComments', child, parent);
-// }
-
-// function _inheritComments(key, child, parent) {
-//   if (child && parent) {
-//     child[key] = uniq([].concat(child[key], parent[key]).filter(Boolean));
-//   }
-// }
-
-// /**
-//  * Inherit all contextual properties from `parent` node to `child` node.
-//  */
-
-// export function inherits(child: Object, parent: Object): Object {
-//   if (!child || !parent) {
-//     return child;
-//   }
-
-//   // optionally inherit specific properties if not null
-//   for (const key of t.INHERIT_KEYS.optional) {
-//     if (child[key] == null) {
-//       child[key] = parent[key];
-//     }
-//   }
-
-//   // force inherit "private" properties
-//   for (const key in parent) {
-//     if (key[0] === '_' && key !== '__clone') {
-//       child[key] = parent[key];
-//     }
-//   }
-
-//   // force inherit select properties
-//   for (const key of t.INHERIT_KEYS.force) {
-//     child[key] = parent[key];
-//   }
-
-//   t.inheritsComments(child, parent);
-
-//   return child;
-// }
-
-// /**
-//  * TODO
-//  */
-
-// export function assertNode(node?) {
-//   if (!isNode(node)) {
-//     // $FlowFixMe
-//     throw new TypeError(`Not a valid node ${node && node.type}`);
-//   }
-// }
-
-// /**
-//  * TODO
-//  */
-
-// export function isNode(node?): boolean {
-//   return !!(node && VISITOR_KEYS[node.type]);
-// }
-
-// // Optimize property access.
-// toFastProperties(t);
-// toFastProperties(t.VISITOR_KEYS);
-
-// /**
-//  * A prefix AST traversal implementation implementation.
-//  */
-
-// export function traverseFast(node: Node, enter: (node: Node) => void, opts?: Object) {
-//   if (!node) {
-//     return;
-//   }
-
-//   const keys = t.VISITOR_KEYS[node.type];
-//   if (!keys) {
-//     return;
-//   }
-
-//   opts = opts || {};
-//   enter(node, opts);
-
-//   for (const key of keys) {
-//     const subNode = node[key];
-
-//     if (Array.isArray(subNode)) {
-//       for (const node of subNode) {
-//         traverseFast(node, enter, opts);
-//       }
-//     } else {
-//       traverseFast(subNode, enter, opts);
-//     }
-//   }
-// }
-
-// const CLEAR_KEYS: Array = ['tokens', 'start', 'end', 'loc', 'raw', 'rawValue'];
-
-// const CLEAR_KEYS_PLUS_COMMENTS: Array = t.COMMENT_KEYS.concat(['comments']).concat(CLEAR_KEYS);
-
-// /**
-//  * Remove all of the _* properties from a node along with the additional metadata
-//  * properties like location data and raw token data.
-//  */
-
-// export function removeProperties(node: Node, opts?: Object): void {
-//   opts = opts || {};
-//   const map = opts.preserveComments ? CLEAR_KEYS : CLEAR_KEYS_PLUS_COMMENTS;
-//   for (const key of map) {
-//     if (node[key] != null) {
-//       node[key] = undefined;
-//     }
-//   }
-
-//   for (const key in node) {
-//     if (key[0] === '_' && node[key] != null) {
-//       node[key] = undefined;
-//     }
-//   }
-
-//   const syms: Array<Symbol> = Object.getOwnPropertySymbols(node);
-//   for (const sym of syms) {
-//     node[sym] = null;
-//   }
-// }
-
-// export function removePropertiesDeep(tree: Node, opts?: Object): Node {
-//   traverseFast(tree, removeProperties, opts);
-//   return tree;
-// }
-
-// //
-// export { getBindingIdentifiers, getOuterBindingIdentifiers } from './retrievers';
-
-// export {
-//   isBinding,
-//   isReferenced,
-//   isValidIdentifier,
-//   isLet,
-//   isBlockScoped,
-//   isVar,
-//   isSpecifierDefault,
-//   isScope,
-//   isImmutable,
-//   isNodesEquivalent
-// } from './validators';
-
-// export {
-//   toComputedKey,
-//   toSequenceExpression,
-//   toKeyAlias,
-//   toIdentifier,
-//   toBindingIdentifierName,
-//   toStatement,
-//   toExpression,
-//   toBlock,
-//   valueToNode
-// } from './converters';
+export function validate(node?: Object, key: string, val: any) {
+  if (!node) {
+    return;
+  }
+
+  const fields = t.NODE_FIELDS[node.type];
+  if (!fields) {
+    return;
+  }
+
+  const field = fields[key];
+  if (!field || !field.validate) {
+    return;
+  }
+  if (field.optional && val == null) {
+    return;
+  }
+
+  field.validate(node, key, val);
+}
+
+/**
+ * Test if an object is shallowly equal.
+ */
+
+export function shallowEqual(actual: Object, expected: Object): boolean {
+  const keys = Object.keys(expected);
+
+  for (const key of keys) {
+    if (actual[key] !== expected[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
