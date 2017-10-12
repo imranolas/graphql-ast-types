783808233018955a42e694edc1c3e34ec9fd5d92
imranolas <imran.sulemanji@gmail.com>
2017-10-12 12:04:20 +0100

    0.1.1

--- generate-docs.js
+++ generate-docs.js
@@ -1,27 +1,4 @@
-// const { parse } = require('babylon');
-// const t = require('babel-types');
-// const visit = require('babel-traverse').default;
 const fs = require('fs');
-// const nodePath = require('path');
-
-// const visitor = {
-//   CallExpression: path => {
-//     if (path.get('callee').isIdentifier({ name: 'defineType' })) {
-//       const typeName = path.get('arguments.0').node.value;
-//       const config = path.get('arguments.1').node.properties;
-//     }
-//   }
-// };
-
-// const raw = fs.readFileSync('./src/definitions/graphql.js', 'utf-8');
-
-// visit(
-//   parse(raw, {
-//     sourceType: 'module',
-//     plugins: ['flow']
-//   }),
-//   visitor
-// );
 
 const docTemplate = (name, { builder, fields }) => {
   fs.appendFileSync(
