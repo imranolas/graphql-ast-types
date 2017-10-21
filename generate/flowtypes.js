127cd0a1b6717e9db12cd95623dc026cdc0a38bc
imranolas <imran.sulemanji@gmail.com>
2017-10-21 21:37:26 +0100

    Generate flow params and return types

--- generate/flowtypes.js
+++ generate/flowtypes.js
@@ -13,30 +13,61 @@ const {
 const makeModuleNode = statements =>
   t.declareModule(t.stringLiteral('babel-ast-types'), t.blockStatement(statements));
 
-const makeTypedIdentifier = (nodeName, annotation) => {
+const makeTypedIdentifier = (nodeName, params, returnAnnotation) => {
   const name = getNodeNameWithoutSuffix(nodeName);
+  const paramNodes = params.map(({ key, value }) =>
+    t.functionTypeParam(t.identifier(key.name), value)
+  );
   return Object.assign(t.identifier(name), {
-    typeAnnotation: t.typeAnnotation(t.functionTypeAnnotation(null, null, null, annotation))
+    typeAnnotation: t.typeAnnotation(
+      t.functionTypeAnnotation(null, paramNodes, null, returnAnnotation)
+    )
   });
 };
 
-const makeDeclaration = (nodeName, node) => [
-  t.declareExportDeclaration(
-    t.declareFunction(
-      makeTypedIdentifier(nodeName, t.genericTypeAnnotation(t.identifier(nodeName)))
+const makeDeclaration = (nodeName, node) => {
+  const args = node.properties.filter(({ key }) => !isPropBlacklisted(key.name)).sort((a, b) => {
+    if (a.optional === b.optional) return 0;
+    if (a.optional) return 1;
+    return -1;
+  });
+
+  const anyNode = [{ key: t.identifier('node'), value: t.anyTypeAnnotation() }];
+
+  return [
+    t.declareExportDeclaration(
+      t.declareFunction(
+        makeTypedIdentifier(nodeName, args, t.genericTypeAnnotation(t.identifier(nodeName)))
+      )
+    ),
+    t.declareExportDeclaration(
+      t.declareFunction(makeTypedIdentifier(`is${nodeName}`, anyNode, t.booleanTypeAnnotation()))
+    ),
+    t.declareExportDeclaration(
+      t.declareFunction(
+        makeTypedIdentifier(`assert${nodeName}`, anyNode, t.booleanTypeAnnotation())
+      )
     )
-  ),
-  t.declareExportDeclaration(
-    t.declareFunction(makeTypedIdentifier(`is${nodeName}`, t.booleanTypeAnnotation()))
-  )
-];
+  ];
+};
+
+makeImportStatement = nodeKeys => {
+  return Object.assign(
+    t.importDeclaration(
+      nodeKeys.map(nodeKey => t.importSpecifier(t.identifier(nodeKey), t.identifier(nodeKey))),
+      t.stringLiteral('graphql/language/ast')
+    ),
+    { importKind: 'type' }
+  );
+};
 
 const buildBabelTemplates = ast => {
   const { nodes, unions } = collectNodes(ast);
   const nodeStatements = Object.keys(nodes)
     .map(nodeName => makeDeclaration(nodeName, nodes[nodeName]))
     .reduce((a = [], ts) => a.concat(ts));
-  return makeModuleNode(nodeStatements);
+
+  return t.program([makeImportStatement(Object.keys(nodes)), makeModuleNode(nodeStatements)]);
 };
 
 const generateDefinitionContent = ast => {
